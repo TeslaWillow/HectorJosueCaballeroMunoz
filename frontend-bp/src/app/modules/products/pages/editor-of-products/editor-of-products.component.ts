c7475b13bbee8aa6a2fe153f7e9f4fb420f5acc4
TeslaWillow <hectorjosue.caballero@gmail.com>
2025-08-03 00:13:40 -0600

    desing of editor page.

--- frontend-bp/src/app/modules/products/pages/editor-of-products/editor-of-products.component.ts
+++ frontend-bp/src/app/modules/products/pages/editor-of-products/editor-of-products.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CardComponent } from '../../../../shared/ui/card/card.component';
+import { ButtonComponent } from '../../../../shared/ui/button/button.component';
+import { InputComponent } from '../../../../shared/components/input/input.component';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'products-editor-of-products',
-  imports: [],
+  imports: [
+    CardComponent,
+    ButtonComponent,
+    InputComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './editor-of-products.component.html',
-  styles: ``
+  styles: ``,
 })
 export default class EditorOfProductsComponent {
+  public readonly title = 'Editor de Productos';
+
+  private _fb = inject(FormBuilder);
+
+  public productForm = this._fb.group({
+    id: ['', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(10),
+      Validators.pattern('^[0-9]+$')
+    ]],
+    name: ['', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(100),
+    ]],
+    description: ['', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(200),
+    ]],
+    logo: ['', [
+      Validators.required
+    ]],
+    date_release: ['', [
+      Validators.required
+    ]],
+    date_revision: ['', [
+      Validators.required
+    ]],
+  });
+
+  public isInvalid(controlName: string): boolean {
+    const control = this.productForm.get(controlName);
+    return control ? control.invalid && (control.dirty || control.touched) : false;
+  }
+
+  public resetForm(): void {
+    this.productForm.reset();
+  }
 
 }
