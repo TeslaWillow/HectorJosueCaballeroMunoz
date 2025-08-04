f55d5def8aaf9ec308b9121c16a9628ef47534a9
TeslaWillow <hectorjosue.caballero@gmail.com>
2025-08-04 04:08:41 -0600

    Added header to the app component.

--- frontend-bp/src/app/app.component.ts
+++ frontend-bp/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ProductHeaderComponent } from './shared/layout/product-header/product-header.component';
 
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterModule,
+    ProductHeaderComponent
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   public title: string = 'frontend-bp';
---
+++ frontend-bp/src/app/shared/layout/product-header/product-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-layout-product-header',
+  templateUrl: './product-header.component.html',
+  styleUrls: ['./product-header.component.scss']
+})
+export class ProductHeaderComponent {}
